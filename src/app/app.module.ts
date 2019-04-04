03fe564b3b4b83aeb904d520c20a86311532079d
TakahiroSono <taka.sono889@gmail.com>
2019-04-04 16:30:30 +0900

    add component of caprion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaptionComponent } from './caption/caption.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaptionComponent
   ],
   imports: [
     BrowserModule,
