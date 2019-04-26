8b24865c4179072274503b81772a431dec6c51da
TakahiroSono <taka.sono889@gmail.com>
2019-04-26 11:20:33 +0900

    rename file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaptionComponent } from './caption/caption.component';
-import { ProfileComponent } from './infomation/profile/profile.component';
-import { LanguageComponent } from './infomation/language/language.component';
-import { LinkComponent } from './infomation/link/link.component';
-import { InfomationComponent } from './infomation/infomation.component';
+import { ProfileComponent } from './information/profile/profile.component';
+import { LanguageComponent } from './information/language/language.component';
+import { LinkComponent } from './information/link/link.component';
+import { InfomationComponent } from './information/infomation.component';
 
 @NgModule({
   declarations: [
