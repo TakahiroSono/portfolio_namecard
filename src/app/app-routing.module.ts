716d823807bd143c95366df20f53b4938ce18555
TakahiroSono <taka.sono889@gmail.com>
2019-04-28 18:21:48 +0900

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './information/profile/profile.component';
+import { LanguageComponent } from './information/language/language.component';
+import { LinkComponent } from './information/link/link.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'language', component: LanguageComponent},
+  {path: 'link', component: LinkComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
