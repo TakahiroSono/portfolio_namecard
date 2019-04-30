e203279e30603d132ba87bd84c641171e1172f57
TakahiroSono <taka.sono889@gmail.com>
2019-04-30 15:08:19 +0900

    add home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LanguageComponent } from './information/language/language.component';
 import { LinkComponent } from './information/link/link.component';
 
 const routes: Routes = [
+  {path: '', component:ProfileComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'language', component: LanguageComponent},
   {path: 'link', component: LinkComponent}
