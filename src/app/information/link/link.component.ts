05ac211e96b77bc98c05c2b6bd9bb926876f4eb9
TakahiroSono <taka.sono889@gmail.com>
2019-05-01 10:19:27 +0900

    add link list

--- src/app/information/link/link.component.ts
+++ src/app/information/link/link.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from './link';
+import { LINKLIST } from './linkList-mock';
 
 @Component({
   selector: 'app-link',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link.component.scss']
 })
 export class LinkComponent implements OnInit {
-
+  links: Link[] = LINKLIST;
   constructor() { }
 
   ngOnInit() {
