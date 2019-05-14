92bfb9f7d762042d43863e363a96f3d2e8516e52
TakahiroSono <taka.sono889@gmail.com>
2019-05-14 20:20:21 +0900

    add animation all page

--- src/app/information/language/language.component.ts
+++ src/app/information/language/language.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { Language } from './language';
 import { LANGUAGE_LIST } from './language-mock';
-import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
+import { trigger, transition, query, style, stagger, animate, useAnimation } from '@angular/animations';
+import { transAnimation } from '../animation';
 
 @Component({
   selector: 'app-language',
@@ -11,18 +12,14 @@ import { trigger, transition, query, style, stagger, animate } from '@angular/an
     trigger('pageAnimations', [
       transition(':enter', [
         query('.language_card',[
-          style({opacity: 0, transform: 'translateX(100px)'}),
-          stagger(-30, [
-            animate('1000ms cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1, transform: 'none'}))
-          ])
+          useAnimation(transAnimation)
         ])
       ])
-    ]),
-  ]
+    ])
+  ],
 })
 export class LanguageComponent implements OnInit {
   @HostBinding('@pageAnimations')
-  // public animatePage = true;
   languages: Language[] = LANGUAGE_LIST;
   constructor() { }
 
